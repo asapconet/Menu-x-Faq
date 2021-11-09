9fe9e8b944ee5e739d3144f3bbc0ad14c43fa12d
asapconet <aaronrickymeek@gmail.com>
2021-11-09 03:14:00 +0100

    testing 456

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainApp = () => {
+
+    return (
+      <>
+        <div>
+          <h1>Lets get to work</h1>
+        </div>
+      </>
+    );
+}
+
+export default MainApp
