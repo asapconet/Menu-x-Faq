5c59a52845e26ed7bfed12517b798ce8b023b88b
asapconet <aaronrickymeek@gmail.com>
2021-11-23 04:35:50 +0100

    proper display implimentation

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button1 from "./button";
+import menu from "../data/api";
 
 export const Card = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [data, setData] = useState(menu);
+
+  return (
+    <div>
+      {data.map((e) => {
+        const { id, title, price, img, desc } = e;
+        return (
+          <div className="flex sm:flex-col " key={id}>
+            <div className="food-card sm:w-40">
+              <h2>{title}</h2>
+              <img src={img} alt={title} />
+              <div className="p-1">
+                <h3 className="">${price}</h3>
+
+                <div className='m-2'>
+                  <Button1>opt1</Button1>
+                  <Button1>optn2</Button1>
+                  <Button1>optn3</Button1>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col text-justify w-80 p-2 m-2 rounded-tl-none rounded-bl-2xl rounded-tr-2xl bg-white">
+              <h4 className="p-1 text-3xl font-bold capitalize text-center">
+                receipe
+              </h4>
+              <p>{desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,38 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+};
+module.exports = {
+  theme: {
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
 };
